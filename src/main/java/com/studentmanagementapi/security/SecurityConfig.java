169eb50b2443244434776613ed7f7ed2a47e1d8d
Mohammed Shakaib <Shakxzz888@gmail.com>
2026-05-10 18:54:37 +0530

    removed duplicate cors bean

--- src/main/java/com/studentmanagementapi/security/SecurityConfig.java
+++ src/main/java/com/studentmanagementapi/security/SecurityConfig.java
@@ -1,14 +1,9 @@
 package com.studentmanagementapi.security;
 
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -32,7 +27,7 @@ public class SecurityConfig {
 
         http
             .csrf(csrf -> csrf.disable())
-            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
+            .cors(cors -> {})
             .authorizeHttpRequests(auth -> auth
             	    .requestMatchers("/auth/**").permitAll()
             	    
@@ -67,36 +62,4 @@ public class SecurityConfig {
 
         return http.build();
     }
-    
-    
-    @Bean
-    public CorsConfigurationSource corsConfigurationSource() {
-
-        CorsConfiguration configuration = new CorsConfiguration();
-
-        configuration.setAllowedOrigins(List.of(
-            "http://localhost:5173",
-            "https://student-frontend-beige.vercel.app"
-        ));
-
-        configuration.setAllowedMethods(List.of(
-            "GET",
-            "POST",
-            "PUT",
-            "DELETE",
-            "OPTIONS"
-        ));
-
-        configuration.setAllowedHeaders(List.of("*"));
-
-        configuration.setAllowCredentials(true);
-
-        UrlBasedCorsConfigurationSource source =
-                new UrlBasedCorsConfigurationSource();
-
-        source.registerCorsConfiguration("/**", configuration);
-
-        return source;
-    }
-    
 }
